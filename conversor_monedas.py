e4138e7436454b6ad09f6a173f2ab3dc3435bbd1
QuirozMonica25 <125092981+QuirozMonica25@users.noreply.github.com>
2025-06-28 19:25:25 -0300

    Update conversor_monedas.py
    "primeros pasos"

--- conversor_monedas.py
+++ conversor_monedas.py
@@ -12,6 +12,26 @@ ventana.configure(bg="LightSteelBlue")
 ventana.resizable(True, False)
 ventana.iconbitmap("Logogrupo 6.ico")
 
+# Pestañas
+notebook = ttk.Notebook(ventana)
+notebook.pack(fill='both',expand='yes')
+
+tab1 = ttk.Frame(notebook)
+tab2 = ttk.Frame(notebook)
+
+notebook.add(tab1, text='Conversor de monedas')
+notebook.add(tab2, text='Lista de monedas')
+
+
+# Diccionario con tasas de cambio simuladas (valores de ejemplo)
+tasas_cambio = {
+    "USD": 1190,
+    "EUR": 1392,
+    "GBP": 1568,
+    "JPY": 10,
+    "AUD": 776,
+    "CAD": 886
+}
 
 
 ventana.mainloop()
